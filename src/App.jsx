af8e0bd67ff38b45eda9011e8d3bb04e66ec30df
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-01-22 08:23:06 +0500

    change state color

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [bgColor, setBgColor] = useState("white");
+  const [bgColor, setBgColor] = useState("#334155");
   const handleChangeColor = (evt) => {
     setBgColor(evt.target.value);
   };
@@ -14,11 +14,11 @@ function App() {
         <h1 className={`text-2xl font-bold ${bgColor === "black" || bgColor === "#334155" || bgColor === "#1e3a8a" ? "text-white"  :"text-black"}`}>Background Color Changer</h1>
         <div>
           <select className="outline-none py-2 px-8 border-2 border-slate-700" id="dropdown" onChange={handleChangeColor}>
+            <option value="#334155">Slate</option>
             <option value="white">White</option>
             <option value="#f472b6">Pink</option>
             <option value="black">Black</option>
             <option value="#2dd4bf">Teal</option>
-            <option value="#334155">Slate</option>
             <option value="#1e3a8a">Blue</option>
             <option value="#fb7185">Rose</option>
             <option value="orange">Orange</option>
